1d0232aaeb1d52520daeb12c9e465f74551ba1f6
wang.yangyang <dyungwang@gmail.com>
2020-04-04 21:25:03 +0800

    tree travel

---
+++ 1376.cc
@@ -0,0 +1,73 @@
+// @author  wangyangyang<wangyanyang@gmail.com>
+// @created 2020/04/04
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+ public:
+  int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+    auto tree = buildTree(manager);
+    return backVist(tree, informTime, headID);
+  }
+ 
+ private:
+  unordered_map<int, vector<int>> buildTree(const vector<int>& manager) {
+    unordered_map<int, vector<int>> tree;
+    for (int i = 0; i < (int)manager.size(); ++i) {
+      tree[manager[i]].push_back(i);
+    }
+    return tree;
+  }
+
+  int backVist(const unordered_map<int, vector<int>>& tree, const vector<int>& informTime, int i) {
+    if (tree.count(i) == 0) 
+      return informTime[i];
+    int time = 0;
+    for (auto j : tree.at(i))
+      time = max(time, backVist(tree, informTime, j));
+    return time + informTime[i];
+  }
+};
+
+struct TestCase {
+  int n;
+  int headID;
+  vector<int> manager;
+  vector<int> informTime;
+  int result;
+};
+
+bool Test(TestCase& testCase) {
+  return Solution().numOfMinutes(
+      testCase.n, 
+      testCase.headID, 
+      testCase.manager, 
+      testCase.informTime) == testCase.result;
+}
+
+TestCase case1 = {  
+  4,
+  2,
+  { 3, 3, -1, 2 },
+  { 0, 0, 162, 914 },
+  1076
+};
+
+TestCase case2 = {
+  7,
+  6,
+  { 1, 2, 3, 4, 5, 6, -1},
+  { 0, 6, 5, 4, 3, 2, 1},
+  21
+};
+
+int main(int, char**) {
+  assert(Test(case1));
+  assert(Test(case2));
+  return 0;
+}
