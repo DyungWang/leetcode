63ffe7ea6ea36701c5cb4f10f6c43c59f569adfd
wang.yangyang <dyungwang@gmail.com>
2020-04-21 11:50:43 +0800

    more readable code

--- 126.cc
+++ 126.cc
@@ -7,7 +7,7 @@
 #include <string>
 #include <unordered_map>
 #include <queue>
-#include <set>
+#include <unordered_set>
 #include "leetcode.h"
 
 using namespace std;
@@ -28,14 +28,12 @@ class Solution {
   }
  
  private:
-  inline void findNext(set<string>& words, string& word, vector<string>* nexts) {
+  inline void findNext(unordered_set<string>& words, string word, vector<string>* nexts) {
     for (size_t i = 0; i < word.size(); ++i) {
       char c = word[i];
       for (char n = 'a'; n <= 'z'; ++n) {
         word[i] = n;
-        if (words.count(word) != 0) {
-          nexts->push_back(word);
-        }
+        if (words.count(word) != 0) nexts->push_back(word);
       }
       word[i] = c;
     }
@@ -44,37 +42,35 @@ class Solution {
   inline int findMinLength(
       string& beginWord, string& endWord, vector<string>& wordList, 
       unordered_map<string, vector<string>>* nextMap) {
-    set<string> words(wordList.begin(), wordList.end());
-    queue<string> que; que.push(beginWord);
+    unordered_set<string> words(wordList.begin(), wordList.end());
+    unordered_set<string> todos = { beginWord };
+    unordered_set<string> nexts;
     int ret = 1;
-    while (!que.empty()) {
-      int len = que.size();
-      for (int i = 0; i < len; ++i) {
-        auto word = que.front(); que.pop();
-        if (word == endWord)
-          return ret;
+    while (!todos.empty()) {
+      if (todos.count(endWord) != 0)
+        return ret;
+      for (auto& word: todos) {
         auto& next = (*nextMap)[word];
         words.erase(word);
         findNext(words, word, &next);
-        for (auto& n : next) que.push(n);
+        nexts.insert(next.begin(), next.end());
       }
+      todos.clear(); swap(todos, nexts);
       ret++;
     }
     return 0;
   }
 
   inline void findMinPaths(
-      int minLength,
-      string& endWord, 
-      vector<string>& path,
-      unordered_map<string, vector<string>>& nextMap,
-      vector<vector<string>>* paths) {
+      int minLength, string& endWord, vector<string>& path,
+      unordered_map<string, vector<string>>& nextMap, vector<vector<string>>* paths) {
     if (minLength == path.size()) {
       if (path.back() == endWord)
         paths->push_back(path);
       return;
     }
-    for (auto& next: nextMap[path.back()]) {
+    auto& nexts = nextMap[path.back()];
+    for (auto& next: nexts) {
       path.push_back(next);
       findMinPaths(minLength, endWord, path, nextMap, paths);
       path.pop_back();
