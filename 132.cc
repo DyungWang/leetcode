d15eed404a0b9a2ff3478582c5a6ada19269599c
wang.yangyang <dyungwang@gmail.com>
2020-04-23 10:04:09 +0800

    132 bfs more effect

--- 132.cc
+++ 132.cc
@@ -30,22 +30,23 @@ class Solution {
 
   inline int bfs(vector<vector<int>>& leftMap) {
     vector<int> visited(leftMap.size()+1, 0);
-    queue<int> todos;
-    todos.push(0);
+    vector<int> todos(1, 0);
+    visited[0] = 1;
     int ret = 0;
     while (!todos.empty()) {
-      int len = todos.size();
-      for (int i = 0; i < len; ++i) {
-        int l = todos.front(); todos.pop();
-        if (l >= leftMap.size())
+      vector<int> nexts;
+      for (auto l : todos) {
+        if (l >= leftMap.size()) 
           return ret;
-        for (auto r : leftMap[l]) if (!visited[r+1])
-          todos.push(r+1);
-        visited[l] = 1;
+        for (auto r : leftMap[l]) if (!visited[r+1]) {
+          nexts.push_back(r+1);
+          visited[r+1] = 1;
+        }
       }
+      swap(todos, nexts);
       ret++;
     }
-    return 0;
+    return ret;
   }
 };
 
