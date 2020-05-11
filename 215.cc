586b00d378ff2d245772604524bdd84f5d58aade
wang.yangyang <dyungwang@gmail.com>
2020-05-11 22:25:00 +0800

    215 nth_element

--- 215.cc
+++ 215.cc
@@ -4,31 +4,16 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
-  typedef vector<int>::iterator Iter;
  public:
   int findKthLargest(vector<int>& nums, int k) {
-    return findKthLargest(nums.begin(), nums.end(), k);
-  }
-
- private:
-  int findKthLargest(Iter begin, Iter end, int k) {
-    Iter l = begin, r = end - 1;
-    while (l < r) {
-      while (*r >  *begin && l < r) --r;
-      while (*l <= *begin && l < r) ++l;
-      swap(*l, *r);
-    }
-    if (*r < *begin) swap(*r, *begin);
-
-    int t = end - r; 
-    if (t == k) return *r;
-    if (t <  k) 
-      return findKthLargest(begin, r, k-t);
-    return findKthLargest(r+1, end, k);
+    auto iter = nums.begin() + nums.size() - k;
+    nth_element(nums.begin(), iter, nums.end());
+    return *iter;
   }
 };
 
