90823be91bacf15fd5c6b51f07bf787fe4bc35c2
wang.yangyang <dyungwang@gmail.com>
2020-05-17 12:42:47 +0800

    282

--- 282.cc
+++ 282.cc
@@ -4,37 +4,59 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include "leetcode.h"
 
 using namespace std;
 
 class Solution {
  public:
-  vector<string> addOperators(string num, int target) {
-    vector<string> res;
-    dfs(num, target, 0, 0, 0, "", res);
-    return res;
-  }
- 
- private:
-  void dfs(string& num, int t, int s, int64_t cv, int64_t pv, string r, vector<string>& res) {
-    if (s == num.size() && cv == t) {
-      res.push_back(r);
-      return;
-    }
-    for (int i = s+1; i <= num.size(); ++i) {
-      if (num[s] == '0' && i-s > 1) 
+  vector<string> addOperatorsWorker(vector<int>::const_iterator I,
+                                  vector<int>::const_iterator E,
+                                  const int64_t target, const char lowOpr,
+                                  const int64_t low, const int64_t high) {
+    vector<string> results;
+    int64_t n = 0;
+    for (auto i = I; i < E; ++i) {
+      // Prevent "00...0" and "0x..x":
+      if (i != I && *I == 0)
         continue;
-      string ns = num.substr(s, i-s);
-      int64_t n = stoll(ns);
-      if (s == 0) {
-        dfs(num, t, i, n, n, ns, res);
-        continue;
+
+      n = n * 10LL + *i;
+      int64_t k = (lowOpr == '+') ? (low + n * high) : (low - n * high);
+      if (i == E - 1) {
+        if (target == k)
+          results.push_back(to_string(n));
+      } else {
+        // For +/-, compute the current accumulated result.
+        // Reset the high-precedence operand.
+        for (auto &&r : addOperatorsWorker(i + 1, E, target, '+', k, 1))
+          results.push_back(to_string(n) + '+' + r);
+        for (auto &&r : addOperatorsWorker(i + 1, E, target, '-', k, 1))
+          results.push_back(to_string(n) + '-' + r);
+
+        // For multiplication, just carry over low-precedence accumulated result.
+        // But, compute the hig-precedence operand now.
+        for (auto &&r : addOperatorsWorker(i + 1, E, target, lowOpr, low, n * high))
+          results.push_back(to_string(n) + '*' + r);
       }
-      dfs(num, t, i, cv+n,  n, r+"+"+ns, res);
-      dfs(num, t, i, cv-n, -n, r+"-"+ns, res);
-      dfs(num, t, i, cv-pv+pv*n, pv*n, r+"*"+ns, res);
     }
+
+    return results;
+  }
+
+  vector<string> addOperators(string num, int target) {
+    if (num.length() == 0)
+      return {};
+
+    // Expand to a digit vector.
+    vector<int> N;
+    for (char c : num)
+      N.push_back(c - '0');
+
+    // "+ 0" and "* 1" for the starting condition; doesn't alter the value.
+    return addOperatorsWorker(N.cbegin(), N.cend(), target, '+', 0, 1);
   }
 };
 
