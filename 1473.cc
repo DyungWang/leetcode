a546e74200a4b559992e326a61987d7f0b1f3316
wang.yangyang <dyungwang@gmail.com>
2020-06-12 10:05:36 +0800

    1473 dfs tle

--- 1473.cc
+++ 1473.cc
@@ -13,8 +13,6 @@ class Solution {
     int ans = dfs(houses, cost, target, 0);
     if (ans == -1) 
       return -1;
-    for (int i = 0; i < houses.size(); ++i)
-      ans += houses[i] == 0 ? 0 : cost[i][houses[i]];
     return ans;
   }
  
@@ -26,23 +24,63 @@ class Solution {
 
     if (houses[i] != 0) {
       int nt = (i != 0 && houses[i-1] == houses[i]) ? target : (target-1);
-      return dfs(houses, paint, cost, nt, i+1);
+      return dfs(houses, cost, nt, i+1);
     }
 
-    int rr = INT_MAX;
+    int rr = -1;
     for (int j = 0; j < cost[i].size(); ++j) {
       houses[i] = j+1;
       int nt = (i != 0 && houses[i-1] == houses[i]) ? target : (target - 1);
-      int nr = dfs(houses, paint, cost, nt, i+1);
+      int nr = dfs(houses, cost, nt, i+1);
       if (nr == -1) continue;
       nr += cost[i][j];
       if (rr == -1 || nr < rr) rr = nr;
     }
     houses[i] = 0;
-    return r;
+    return rr;
   }
 };
 
 struct TestCase {
-  
+  vector<int> houses;
+  vector<vector<int>> cost;
+  int m;
+  int n;
+  int target;
+  int res;
+
+  bool test() { return res == Solution().minCost(houses, cost, m, n, target); }
 };
+
+TestCase testCase1 = {
+  {0,0,0,0,0},
+  {{1,10},{10,1},{10,1},{1,10},{5,1}},
+  5, 2, 3, 9
+};
+
+TestCase testCase2 = {
+  {0,2,1,2,0},
+  {{1,10},{10,1},{10,1},{1,10},{5,1}},
+  5, 2, 3, 11
+};
+
+TestCase testCase3 = {
+  {0,0,0,0,0},
+  {{1,10},{10,1},{1,10},{10,1},{1,10}},
+  5, 2, 5, 5
+};
+
+TestCase testCase4 = {
+  {3,1,2,3},
+  {{1,1,1},{1,1,1},{1,1,1},{1,1,1}},
+  4, 3, 3, -1
+};
+
+int main(int, char**) {
+  #define TEST(testCase) assert(testCase.test())
+  TEST(testCase1);
+  TEST(testCase2);
+  TEST(testCase3);
+  TEST(testCase4);
+  return 0;
+}
