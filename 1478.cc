ed73d1df01dbe9c8491e0a5f1d6cb369b2b95779
wang.yangyang <dyungwang@gmail.com>
2020-06-17 20:32:08 +0800

    1478

---
+++ 1478.cc
@@ -0,0 +1,14 @@
+// @author  wangyangyang<wangyanyang@gmail.com>
+// @created 2020/06/17
+// @link https://leetcode.com/problems/allocate-mailboxes/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int minDistance(vector<int>& houses, int k) {
+    
+  }
+};
